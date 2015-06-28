e2331ddc9eb10f5f85945cc6100d1a484a854cd5
Phillip Birtcher <philbirt@gmail.com>
2015-06-27 21:55:21 -0400

    Add typekit, make default fonts for body and headers

--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,6 @@ ActiveRecord::Schema.define(version: 20150626221504) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "first_name"
-    t.string   "last_name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
