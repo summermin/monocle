aacf502305fe8493d5bf1904e30f61e4b9a08ed9
Phillip Birtcher <philbirt@gmail.com>
2016-06-25 18:37:32 -0400

    Deliver now, not later

--- app/models/email_notification.rb
+++ app/models/email_notification.rb
@@ -2,6 +2,6 @@ class EmailNotification < ActiveRecord::Base
   validates :email, :project_id, presence: true
 
   def notify
-    ProjectNotificationMailer.checkin_email(to_address: email).deliver_later
+    ProjectNotificationMailer.checkin_email(to_address: email).deliver_now
   end
 end
